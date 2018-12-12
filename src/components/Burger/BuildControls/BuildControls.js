7b87fbd22642917050679fcab0eb3cd60c3c5158
Rahul Zore <zore@synopsys.com>
2018-12-12 15:20:38 -0500

    Day 2 of react burger app

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,9 +11,16 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={styles.BuildControls}>
+        <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
-             <BuildControl key={ctrl.label} label={ctrl.label} added={() => props.ingredientAdded(ctrl.type)}/>
+             <BuildControl 
+             key={ctrl.label} 
+             label={ctrl.label} 
+             added={() => props.ingredientAdded(ctrl.type)} 
+             removed={() => props.ingredientRemoved(ctrl.type)}
+             disabled={props.disabled[ctrl.type]}/>
         ))}
+        <button className={styles.OrderButton} disabled={!props.purchasable} onClick={props.ordered}>ORDER NOW</button>
     </div>
 );
 
